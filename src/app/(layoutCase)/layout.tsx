8ef0a25a970aaacf03e92d2831100cc87214c55c
nam-yeun-hwa <nyh2ya@gmail.com>
2023-12-20 01:17:04 +0900

    design(layout):sidebar 메뉴 추가 및 마우스 오버 추가

--- src/app/(layoutCase)/layout.tsx
+++ src/app/(layoutCase)/layout.tsx
@@ -1,5 +1,7 @@
+'use client';
 import Link from 'next/link';
 import style from './layout.module.css';
+import { useSelectedLayoutSegment } from 'next/navigation';
 
 export default function Layout({
   children,
@@ -8,6 +10,8 @@ export default function Layout({
   children: React.ReactNode;
   modal: React.ReactNode;
 }) {
+  const segment = useSelectedLayoutSegment();
+
   return (
     <>
       <aside id="sidebar" className={style.sidebar}>
@@ -18,6 +22,18 @@ export default function Layout({
               <Link className={style.nav_link} href={'http://www.naver.com'}>
                 <i className={`fa-fw fas fa-home ${style.ico}`}></i>
                 <span className={style.nav_txt}>HOME</span>
+              </Link>
+            </li>
+            <li className={style.nav_item}>
+              <Link className={style.nav_link} href={'http://www.naver.com'}>
+                <i className={`fa-fw fas fa-archive ${style.ico}`}></i>
+                <span className={style.nav_txt}>ARCHIVES</span>
+              </Link>
+            </li>
+            <li className={style.nav_item}>
+              <Link className={style.nav_link} href={'http://www.naver.com'}>
+                <i className={`fa-fw fas fa-info-circle ${style.ico}`}></i>
+                <span className={style.nav_txt}>ABOUT</span>
               </Link>
             </li>
           </ul>
