8913b59c252e9c4a82c73df84dfb58e6da965e8c
nam-yeun-hwa <nyh2ya@gmail.com>
2023-12-29 23:55:12 +0900

    Design(Post, posts):포스트 컴포넌트 및 포스트 페이지 css 추가

--- src/app/(layoutCase)/_component/Post.tsx
+++ src/app/(layoutCase)/_component/Post.tsx
@@ -14,7 +14,17 @@ export default function Post() {
               Site There are two wa
             </p>
           </div>
-          <div></div>
+          <div className={style.post_meta}>
+            <div className={style.post_info}>
+              <i className={`far fa-calendar fa-fw ${style.ico_calendar}`}></i>
+              <time className={style.time}>Aug 9, 2019</time>
+              <i className={`far fa-folder-open fa-fw ${style.ico_folder}`}></i>
+              <span className={style.categories}> Blogging, Tutorial </span>
+            </div>
+            <div className={style.pin}>
+              <i className={`fas fa-thumbtack fa-fw ${style.ico_pin}`}></i>
+            </div>
+          </div>
         </div>
       </Link>
     </article>
