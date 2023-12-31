eeccb608f7e23ccc6d8c0d92b3c4d84e6263c453
nam-yeun-hwa <nyh2ya@gmail.com>
2023-12-31 23:39:42 +0900

    Design(Post, post):상단 breadcrumb 퍼블리싱 추가 및 post 페이지 자잘하게 퍼블리싱  수정

--- src/app/(layoutCase)/posts/page.tsx
+++ src/app/(layoutCase)/posts/page.tsx
@@ -4,20 +4,20 @@ import style from './page.module.css';
 export default function posts() {
   return (
     <>
-      <header>
-        <div className="top_bar">
-          <nav>
-            <span>HOME</span>
+      <header className={style.topbar_wrapper}>
+        <div className={style.top_bar}>
+          <nav className={style.breadcrumb}>
+            <span>Home</span>
           </nav>
-          <search>
-            <i></i>
-            <input />
+          <search className={style.search}>
+            <i className={`fas fa-search fa-fw ${style.search_ico}`}></i>
+            <input className={style.search_input} placeholder="Search..." autoComplete="off" />
           </search>
         </div>
       </header>
       <div className={style.contents}>
         <main className={style.main_wrapper}>
-          <div className="post-list">
+          <div className={style.post_list}>
             <Post />
           </div>
         </main>
