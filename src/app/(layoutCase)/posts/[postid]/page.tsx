65f2d4d7ec71bb9dedb4a448ddf1c1835d5de337
nam-yeun-hwa <nyh2ya@gmail.com>
2024-01-06 01:30:16 +0900

    Design(post detail):포스트 상세페이지 퍼블리싱

--- src/app/(layoutCase)/posts/[postid]/page.tsx
+++ src/app/(layoutCase)/posts/[postid]/page.tsx
@@ -1,3 +1,31 @@
+import BreadCrumb from 'app/_component/BreadCrumb';
+import style from './page.module.css';
+
 export default function Post() {
-  return <div>포스트 상세페이지 </div>;
+  return (
+    <>
+      <BreadCrumb />
+      <div className={style.contents}>
+        <main className={style.main_content}>
+          <article className={style.detail}>
+            <header>
+              <h1 className={style.h1}>Text and Typography</h1>
+            </header>
+          </article>
+          <div className={style.post_meta}>
+            <span>
+              Posted
+              <time>Aug 8, 2019</time>
+            </span>
+          </div>
+          <div className={style.content}>
+            상세페이지상세페이지상세페이지상세페이지상세페이지상세페이지상세페이지상세페이지상세페이지상세페이지상세페이지상세페이지상세페이지상세페이지상세페이지상세페이지상세페이지
+          </div>
+        </main>
+        <aside className={style.panel_wrapper}>
+          <div style={{ width: '100%' }}>flasjf akjf l</div>
+        </aside>
+      </div>
+    </>
+  );
 }
