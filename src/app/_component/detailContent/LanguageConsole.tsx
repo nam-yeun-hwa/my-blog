deeae7c9a09475ee9179f70e8ff07bc65964ac50
nam-yeun-hwa <nyh2ya@gmail.com>
2024-01-09 00:20:46 +0900

    Design(LanguageConsole): languageConsole 코드 내용 추가

--- src/app/_component/detailContent/LanguageConsole.tsx
+++ src/app/_component/detailContent/LanguageConsole.tsx
@@ -2,16 +2,32 @@ import style from './LanguageConsole.module.css';
 
 export default function LanguageConsole() {
   return (
-    <div className={style.code_container}>
-      <div className={style.code_header}>
-        <span className={style.title} data-label-text="Console">
+    <div className={style.console_container}>
+      <div className={style.console_header}>
+        <span className={style.title} data-label-text="Javascript">
           <i className={`fas fa-code fa-fw small ${style.ico_title}`}></i>
         </span>
         <button className={style.copy}>
           <i className={`far fa-clipboard ${style.ico_clipboard}`}></i>
         </button>
       </div>
-      <div className="highlight"></div>
+      <div className={style.console_content}>
+        <pre
+          className={style.console_code}
+          dangerouslySetInnerHTML={{
+            __html: `const info={
+  id:1,
+  name:'John'
+}
+            
+const nextInfo={
+  ...info,
+  name: 'Jane'
+}
+`,
+          }}
+        ></pre>
+      </div>
     </div>
   );
 }
