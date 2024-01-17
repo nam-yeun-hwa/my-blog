e3677728bf5566bd585bef8f4cb9c96520aedd72
nam-yeun-hwa <nyh2ya@gmail.com>
2024-01-17 20:58:57 +0900

    Comment(post list): 무한스크롤 내용 주석 추가

--- src/app/_component/Home.tsx
+++ src/app/_component/Home.tsx
@@ -3,13 +3,10 @@
 import Post from 'app/(layoutCase)/_component/Post';
 import BreadCrumb from './common/BreadCrumb';
 import style from './Home.module.css';
-
 import { useContext, useEffect } from 'react';
 import { PostContext } from 'app/(layoutCase)/_component/contexts/PostContextProvider';
 import { totalPostlist } from 'data/db';
-
 import { InView, useInView } from 'react-intersection-observer';
-import { Folder, IPost } from 'type/post';
 
 export default function Home() {
   const { postList, setPostList } = useContext(PostContext);
@@ -19,6 +16,10 @@ export default function Home() {
     delay: 0,
   });
 
+  // 전체 포스트 리스트 totalPostlist의 값과 현재 화면에 렌더링 된 postList의 값을
+  // 비교하여 전체 리스트의 값을 넘지 않았을 경우에만 postList에 값을 추가 하도록 하며
+  // 리스트를 추가 해줄때 현재 렌더링된 리스트의 마지막 id 값을 기준으로 5개씩 화면에 추가
+  // 하도록 한다.
   useEffect(() => {
     if (inView && postList.length < totalPostlist.length) {
       setPostList(
