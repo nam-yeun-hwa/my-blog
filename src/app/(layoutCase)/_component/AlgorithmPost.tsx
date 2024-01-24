5c5c55fdd133d75f55b86cf3ad31bc3b8981c159
nam-yeun-hwa <nyh2ya@gmail.com>
2024-01-24 18:26:27 +0900

    Design(AlgorithmPost):html 구조 변경

--- src/app/(layoutCase)/_component/AlgorithmPost.tsx
+++ src/app/(layoutCase)/_component/AlgorithmPost.tsx
@@ -17,12 +17,12 @@ export default function AlgorithmPost({ post }: Props) {
             <div className={style.post_info}>
               <i className={`far fa-calendar fa-fw ${style.ico_calendar}`}></i>
               <time className={style.time}>{post.date}</time>
-              <i className={`far fa-folder-open fa-fw ${style.ico_folder}`}></i>
-              <span className={style.categories}> {post.folder} </span>
               <i className={`fa-solid fa-layer-group ${style.ico_folder}`}></i>
               <span className={style.categories}> {post.lv} </span>
               <i className={`fa-solid fa-sitemap ${style.ico_folder}`}></i>
               <span className={style.categories}> 프로그래머스 </span>
+              <i className={`far fa-folder-open fa-fw ${style.ico_folder}`}></i>
+              <span className={style.categories}> {post.folder} </span>
             </div>
             <div className={style.pin}>
               <i className={`fas fa-thumbtack fa-fw ${style.ico_pin}`}></i>
