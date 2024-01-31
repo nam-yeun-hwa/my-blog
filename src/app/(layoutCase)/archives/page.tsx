702feea0740ffb7ab3d924f46caedf9c3737085b
nam-yeun-hwa <nyh2ya@gmail.com>
2024-01-31 15:14:19 +0900

    Design(category, archives, tags):각페이지 퍼블리싱

--- src/app/(layoutCase)/archives/page.tsx
+++ src/app/(layoutCase)/archives/page.tsx
@@ -1,3 +1,37 @@
+import PageHeading from 'app/_component/common/PageHeading';
+import style from './page.module.css';
+import Link from 'next/link';
+
 export default function archives() {
-  return <>archives</>;
+  return (
+    <article>
+      <PageHeading pageTitle="Tags" />
+      <div className="content archives">
+        <time className={style.year}>2019</time>
+        <ul className={style.list_unstyled}>
+          <li className={style.list_item}>
+            <span className={style.day}>1 1</span>
+            <span className={style.month}>Aug</span>
+            <Link className={style.list_link} href="">
+              Customize the Favicon
+            </Link>
+          </li>
+          <li className={style.list_item}>
+            <span className={style.day}>09</span>
+            <span className={style.month}>Aug</span>
+            <Link className={style.list_link} href="">
+              Getting Started
+            </Link>
+          </li>
+          <li className={style.list_item}>
+            <span className={style.day}>08</span>
+            <span className={style.month}>Aug</span>
+            <Link className={style.list_link} href="">
+              Text and Typography
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </article>
+  );
 }
--- src/app/(layoutCase)/tags/page.tsx
+++ src/app/(layoutCase)/tags/page.tsx
@@ -1,3 +1,45 @@
+import PageHeading from 'app/_component/common/PageHeading';
+import style from './page.module.css';
+import Link from 'next/link';
+
 export default function tags() {
-  return <>tags</>;
+  return (
+    <article>
+      <PageHeading pageTitle="Tags" />
+      <div className="content">
+        <div className={style.tag_container}>
+          <div className="tag_item">
+            <Link className={style.tag_item_link} href="">
+              favicon
+              <span className={style.text_muted}>1</span>
+            </Link>
+          </div>
+          <div className="tag_item">
+            <Link className={style.tag_item_link} href="">
+              favicon
+              <span className={style.text_muted}>1</span>
+            </Link>
+          </div>
+          <div className="tag_item">
+            <Link className={style.tag_item_link} href="">
+              favicon
+              <span className={style.text_muted}>1</span>
+            </Link>
+          </div>
+          <div className="tag_item">
+            <Link className={style.tag_item_link} href="">
+              favicon
+              <span className={style.text_muted}>1</span>
+            </Link>
+          </div>
+          <div className="tag_item">
+            <Link className={style.tag_item_link} href="">
+              favicon
+              <span className={style.text_muted}>1</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
 }
