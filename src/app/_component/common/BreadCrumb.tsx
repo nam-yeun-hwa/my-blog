098e0fe1e9c3ac22f5f6a0e51cb96259c37180a3
nam-yeun-hwa <nyh2ya@gmail.com>
2024-02-07 19:17:49 +0900

    Feat(BreadCrumb):상위 카테고리가 알고리즘일때와 아닐때 현재 페이지에 따른 분기 렌더링

--- src/app/_component/common/BreadCrumb.tsx
+++ src/app/_component/common/BreadCrumb.tsx
@@ -1,16 +1,62 @@
 'use client';
 
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import style from './breadCrumb.module.css';
 import cx from 'classnames';
-import { useSelectedLayoutSegments } from 'next/navigation';
+import {
+  useSelectedLayoutSegment,
+  useSelectedLayoutSegments,
+} from 'next/navigation';
 import Link from 'next/link';
 import { totalPostlist } from 'data/post_db';
+import { algorithmPostlist } from 'data/algorithm_db';
+import { IAlgorithmPost, IPost } from 'type/post';
 
+/**
+ * @function AllPageRouter
+ * @description 전체라우터 표시
+ */
+function AllPageRouter() {
+  const allSegment = useSelectedLayoutSegments();
+  return (
+    <>
+      {allSegment.map((linkName, idx) => {
+        return (
+          <Link key={idx} className={style.topbar_txt} href={``}>
+            {linkName.charAt(0).toUpperCase() + linkName.slice(1)}
+          </Link>
+        );
+      })}
+    </>
+  );
+}
+
+/**
+ * @function CurrentPageRouter
+ * @description 현재 페이지 라우터
+ */
+type Props = {
+  data: IPost | IAlgorithmPost;
+};
+function CurrentPageRouter({ data }: Props) {
+  return (
+    <>
+      <Link className={style.topbar_txt} href={``}>
+        {data.title}
+      </Link>
+    </>
+  );
+}
+
+/**
+ * @function BreadCrumb
+ * @description BreadCrumb nav default 라우터
+ */
 export default function BreadCrumb() {
   const [isFocused, setFocused] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
   const allSegment = useSelectedLayoutSegments();
+  const menuRouter = useSelectedLayoutSegment();
 
   const handleFocus = () => {
     setFocused(true);
@@ -28,21 +74,26 @@ export default function BreadCrumb() {
             <Link className={style.topbar_txt} href={``}>
               Home
             </Link>
-            {Number(allSegment[allSegment.length - 1]) > 0 ? (
-              <Link className={style.topbar_txt} href={``}>
-                {
-                  totalPostlist[Number(allSegment[allSegment.length - 1]) - 1]
-                    .title
+            {menuRouter !== 'algorithm' ? (
+              Number(allSegment[allSegment.length - 1]) > 0 ? (
+                <CurrentPageRouter
+                  data={
+                    totalPostlist[Number(allSegment[allSegment.length - 1]) - 1]
+                  }
+                />
+              ) : (
+                <AllPageRouter />
+              )
+            ) : Number(allSegment[allSegment.length - 1]) > 0 ? (
+              <CurrentPageRouter
+                data={
+                  algorithmPostlist[
+                    Number(allSegment[allSegment.length - 1]) - 1
+                  ]
                 }
-              </Link>
+              />
             ) : (
-              allSegment.map((linkName, idx) => {
-                return (
-                  <Link key={idx} className={style.topbar_txt} href={``}>
-                    {linkName.charAt(0).toUpperCase() + linkName.slice(1)}
-                  </Link>
-                );
-              })
+              <AllPageRouter />
             )}
           </span>
         </nav>
@@ -74,6 +125,23 @@ export default function BreadCrumb() {
     </header>
   );
 }
+
+// Number(allSegment[allSegment.length - 1]) > 0 ? (
+//   <Link className={style.topbar_txt} href={``}>
+//     {
+//       totalPostlist[Number(allSegment[allSegment.length - 1]) - 1]
+//         .title
+//     }
+//   </Link>
+// )
+
+// allSegment.map((linkName, idx) => {
+//   return (
+//     <Link key={idx} className={style.topbar_txt} href={``}>
+//       {linkName.charAt(0).toUpperCase() + linkName.slice(1)}
+//     </Link>
+//   );
+// })
 
 /* "Breadcrumb"은 길을 잃지 않도록 도와주는 나침반과 같은 역할을 하는 정보를 나타냅니다. 
   특히 웹 디자인 및 사용자 인터페이스에서 "breadcrumb"은 사용자가 현재 위치한 페이지의 경로를 
