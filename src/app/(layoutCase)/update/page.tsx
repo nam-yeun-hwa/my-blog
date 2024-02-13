99d8bfef912462eb2a892d4033c9d535dc678718
nam-yeun-hwa <nyh2ya@gmail.com>
2024-02-13 23:35:04 +0900

    comment(Footer):하단 버전적용 및 update 페이지 복원

---
+++ src/app/(layoutCase)/update/page.tsx
@@ -0,0 +1,110 @@
+export default function about() {
+  return (
+    <div>
+      <h1>페이지 업데이트 내용</h1>
+      <div className="row">
+        <p>현재 추후 퍼블리싱 추가</p>
+      </div>
+      <b> 2023-12-14 </b> start
+      <br />
+      <br />
+      <b> 2023-12-14 </b> 프로젝트 생성 <br />
+      라우터그룹(layoutCase)에 페이지와 parallel 모달 내용 구성
+      <br />
+      <br />
+      <b> 2023-12-18 </b> <br />
+      sidebar, header 등 공통 레이아웃 구성 추가
+      <br />
+      <br />
+      <b> 2023-12-21 </b> <br />
+      side메뉴 링크페이지 연결 ,프로필사진 추가, 프로필 소개 내용 추가
+      <br />
+      <br />
+      <b> 2023-12-26 </b> <br />
+      포스트 페이지, post 공통 컴포넌트 추가
+      <br />
+      <br />
+      <b> 2023-12-28 </b> <br />
+      각 페이지 생성(HOME, CATEGORY, TAGS, ARCHIVES 등..)
+      <br />
+      <br />
+      <b> 2023-12-31 </b> <br />
+      breadcrumb 공통 컴포넌트 추가
+      <br />
+      <br />
+      <b> 2024-01-04 </b> <br />
+      breadcrumb search 컴포넌트 추가
+      <br />
+      <br />
+      {/* <b> 2023-01-06 </b> <br />
+      포스트 상세페이지 시작
+      <br />
+      <br /> */}
+      <b>2023-01-06 </b> <br />
+      구글 폰트 적용(IBM Plex Sans KR)
+      <br />
+      <br />
+      <b> 2023-01-07 </b> <br />
+      post 상세페이지 코드 공통 컴포넌트 생성
+      <br />
+      <br />
+      <b> 2023-01-09 </b> <br />
+      prompts 컴포넌트 추가
+      <br />
+      <br />
+      <b> 2023-01-07 </b> <br />
+      post 상세페이지 코드 공통 컴포넌트 생성
+      <br />
+      <br />
+      <b> 2023-01-15 </b> <br />
+      전역 상태 관리를 위해 Context API 추가
+      <br />
+      <br />
+      <b> 2023-01-17 </b> <br />
+      post 페이지 5개 게시물 단위로 무한 스크롤 기능 추가
+      <br />
+      react-intersection-observer 사용
+      <br />
+      <br />
+      <b> 2023-01-19 </b> <br />
+      패널 공톰 컴포넌트 생성
+      <br />
+      <br />
+      <b> 2023-01-20 </b> <br />
+      Context API를 reduct-toolkit으로 변경
+      <br />
+      <br />
+      <b> 2023-01-24 </b> <br />
+      알고리즘 카테고리 생성
+      <br />
+      <br />
+      <b> 2023-01-30 </b> <br />
+      카테고리 페이지 개발
+      <br />
+      <br />
+      <b> 2023-02-03 </b> <br />
+      아카이브 카테고리 개발
+      <br />
+      <br />
+      <b> 2023-02-06 </b> <br />
+      태그 카테고리 개발
+      <br />
+      <br />
+      <b> 2023-02-07 </b> <br />
+      BreadCrumb 라우터 경로 컴포넌트 개발
+      <br />
+      <br />
+      <b> 2023-02-12 </b> <br />
+      github.io 배포
+      <br />
+      <br />
+      <b> 2023-02-12 </b> <br />
+      github.io 배포
+      <br />
+      <br />
+      <b> 2023-02-13 </b> <br />
+      기본 배포버전 적용 v1.0.0
+    </div>
+  );
+}
+// v1.0.0
--- src/app/_component/common/Footer.tsx
+++ src/app/_component/common/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
         <p className={style.copylight}>
           © 2024 bunny Chung. Some rights reserved.
         </p>
-        <p className={style.theme}>Using the theme ...</p>
+        <p className={style.theme}>v1.0.0</p>
       </footer>
     </div>
   );
