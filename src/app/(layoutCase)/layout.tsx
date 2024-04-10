b50003a49ed40d5c889cceea9fa9b1c55ed71a93
nam-yeun-hwa <nyh2ya@gmail.com>
2024-04-10 22:08:39 +0900

    refactor(SideMenu): 사이드메뉴 컴포넌트로 분리

--- src/app/(layoutCase)/layout.tsx
+++ src/app/(layoutCase)/layout.tsx
@@ -11,6 +11,7 @@ import { RootState, store } from 'store/index';
 import ImageLoader from 'app/_component/common/ImageLoader';
 import { MOBILE_WIDTH } from 'app/_component/constant/constant';
 import useWindowSize from 'hook/useWindowSize';
+import SideMenu from 'app/_component/common/SideMenu';
 
 type Props = {
   children: ReactNode;
@@ -18,27 +19,12 @@ type Props = {
 };
 
 function Layout({ children, modal }: Props) {
-  const segment = useSelectedLayoutSegment();
   const [onToggle, setOnToggle] = useState(false);
-  // const [browserWidth, setBrowserWidth] = useState<null | number>(null);
   const windowSize = useWindowSize();
-  // /**
-  //  * @description 현재 브라우저 사이즈
-  //  */
-  // useEffect(() => {
-  //   const updateWidth = () => {
-  //     setBrowserWidth(window.innerWidth);
-  //   };
-  //   updateWidth();
-  //   window.addEventListener('resize', updateWidth);
-  //   return () => {
-  //     window.removeEventListener('resize', updateWidth);
-  //   };
-  // }, []);
 
   /**
    * @function moveToggle
-   * @description 모바일사이즈보다 브라우저 사이즈가 작을 경우 sideMenu 안으로 숨기기
+   * @description 모바일사이즈일때 sideMenu 안으로 숨기기
    */
   const moveToggle = () => {
     if (
@@ -51,93 +37,8 @@ function Layout({ children, modal }: Props) {
 
   return (
     <>
-      <aside
-        id="sidebar"
-        className={cx(style.sidebar, onToggle && style.sidebar_move)}
-      >
-        <header className={style.profile_wrapper}>
-          <Link href={'/posts'}>
-            <ImageLoader />
-          </Link>
-          <h1 className={style.blog_name_txt}>my blog</h1>
-          <p className={style.site_subtitle}>
-            You can only earn as much as an ant does by working like an ant.
-          </p>
-        </header>
-        <nav>
-          <ul className={style.nav}>
-            <li
-              className={cx(
-                style.nav_item,
-                (segment === 'posts' || segment === null) && style.active,
-              )}
-              onClick={moveToggle}
-            >
-              <Link href={'/posts'} className={style.nav_link}>
-                <i className={`fa-fw fas fa-home ${style.ico}`}></i>
-                <span className={style.nav_txt}>HOME</span>
-              </Link>
-            </li>
-            <li
-              className={cx(
-                style.nav_item,
-                segment === 'categories' && style.active,
-              )}
-              onClick={moveToggle}
-            >
-              <Link className={style.nav_link} href={'/categories'}>
-                <i className={`fa-fw fas fa-stream ${style.ico}`}></i>
-                <span className={style.nav_txt}>CATEGORIES</span>
-              </Link>
-            </li>
-            <li
-              className={cx(style.nav_item, segment === 'tags' && style.active)}
-              onClick={moveToggle}
-            >
-              <Link className={style.nav_link} href={'/tags'}>
-                <i className={`fa-fw fas fa-tags ${style.ico}`}></i>
-                <span className={style.nav_txt}>TAGS</span>
-              </Link>
-            </li>
-            <li
-              className={cx(
-                style.nav_item,
-                segment === 'archives' && style.active,
-              )}
-              onClick={moveToggle}
-            >
-              <Link className={style.nav_link} href={'/archives'}>
-                <i className={`fa-fw fas fa-archive ${style.ico}`}></i>
-                <span className={style.nav_txt}>ARCHIVES</span>
-              </Link>
-            </li>
-            <li
-              className={cx(
-                style.nav_item,
-                segment === 'algorithm' && style.active,
-              )}
-              onClick={moveToggle}
-            >
-              <Link className={style.nav_link} href={'/algorithm'}>
-                <i className={`fa-solid fa-mug-saucer ${style.ico}`}></i>
-                <span className={style.nav_txt}>CODING</span>
-              </Link>
-            </li>
-            {/* <li
-              className={cx(
-                style.nav_item,
-                segment === 'about' && style.active,
-              )}
-            >
-              <Link className={style.nav_link} href={'/update'}>
-                <i className={`fa-fw fas fa-info-circle ${style.ico}`}></i>
-                <span className={style.nav_txt}>UPDATE</span>
-              </Link>
-            </li> */}
-          </ul>
-        </nav>
-        <div className="sidebar-bottom"></div>
-      </aside>
+      <SideMenu onToggle={onToggle} moveToggle={moveToggle} />
+
       <Provider store={store}>
         <div className={cx(style.main_wrapper, onToggle && style.main_move)}>
           <div className={style.container}>
---
+++ src/app/_component/common/SideMenu.tsx
@@ -0,0 +1,94 @@
+'use client';
+import Link from 'next/link';
+import style from './sidemenu.module.css';
+
+import cx from 'classnames';
+import ImageLoader from 'app/_component/common/ImageLoader';
+import { useSelectedLayoutSegment } from 'next/navigation';
+
+type Props = {
+  onToggle: boolean;
+  moveToggle: () => void;
+};
+
+export default function SideMenu({ onToggle, moveToggle }: Props) {
+  const segment = useSelectedLayoutSegment();
+  return (
+    <aside
+      id="sidebar"
+      className={cx(style.sidebar, onToggle && style.sidebar_move)}
+    >
+      <header className={style.profile_wrapper}>
+        <Link href={'/posts'}>
+          <ImageLoader />
+        </Link>
+        <h1 className={style.blog_name_txt}>my blog</h1>
+        <p className={style.site_subtitle}>
+          You can only earn as much as an ant does by working like an ant.
+        </p>
+      </header>
+      <nav>
+        <ul className={style.nav}>
+          <li
+            className={cx(
+              style.nav_item,
+              (segment === 'posts' || segment === null) && style.active,
+            )}
+            onClick={moveToggle}
+          >
+            <Link href={'/posts'} className={style.nav_link}>
+              <i className={`fa-fw fas fa-home ${style.ico}`}></i>
+              <span className={style.nav_txt}>HOME</span>
+            </Link>
+          </li>
+          <li
+            className={cx(
+              style.nav_item,
+              segment === 'categories' && style.active,
+            )}
+            onClick={moveToggle}
+          >
+            <Link className={style.nav_link} href={'/categories'}>
+              <i className={`fa-fw fas fa-stream ${style.ico}`}></i>
+              <span className={style.nav_txt}>CATEGORIES</span>
+            </Link>
+          </li>
+          <li
+            className={cx(style.nav_item, segment === 'tags' && style.active)}
+            onClick={moveToggle}
+          >
+            <Link className={style.nav_link} href={'/tags'}>
+              <i className={`fa-fw fas fa-tags ${style.ico}`}></i>
+              <span className={style.nav_txt}>TAGS</span>
+            </Link>
+          </li>
+          <li
+            className={cx(
+              style.nav_item,
+              segment === 'archives' && style.active,
+            )}
+            onClick={moveToggle}
+          >
+            <Link className={style.nav_link} href={'/archives'}>
+              <i className={`fa-fw fas fa-archive ${style.ico}`}></i>
+              <span className={style.nav_txt}>ARCHIVES</span>
+            </Link>
+          </li>
+          <li
+            className={cx(
+              style.nav_item,
+              segment === 'algorithm' && style.active,
+            )}
+            onClick={moveToggle}
+          >
+            <Link className={style.nav_link} href={'/algorithm'}>
+              <i className={`fa-solid fa-mug-saucer ${style.ico}`}></i>
+              <span className={style.nav_txt}>CODING</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="sidebar-bottom"></div>
+    </aside>
+  );
+}
