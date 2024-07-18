1365d93f17f80924c5c80960e94533776ae9f4c4
nyh <nyh2ya@gmail.com>
2024-07-18 11:02:11 +0900

    Update(post_db):tdd 내용 추가

--- src/app/_component/detailPage/Keyword.tsx
+++ src/app/_component/detailPage/Keyword.tsx
@@ -1,19 +1,26 @@
-"use client"
+'use client';
 
 import { useEffect } from 'react';
-import style from './Keyword.module.css'
+import style from './Keyword.module.css';
 
 type Props = {
-    keyword: string;
-  };
+	keyword: string;
+};
 
-export default  function KeywordWord({ keyword }: Props){
-
-    return <div className={style.container}>
-        <p className={style.keywordTilte}>관련 키워드</p>
-        <div className={style.keywordContainer}>{keyword.split(",").map((string, id) => {
-        return <p key={id} className={style.keyword}>{string}</p>
-    })}</div>
-        </div> 
-
+export default function KeywordWord({ keyword }: Props) {
+	useEffect(() => {}, []);
+	return (
+		<div className={style.container}>
+			<p className={style.keywordTilte}>관련 키워드</p>
+			<div className={style.keywordContainer}>
+				{keyword.split(',').map((string, id) => {
+					return (
+						<p key={id} className={style.keyword}>
+							{string}
+						</p>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
