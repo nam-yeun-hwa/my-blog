c72a715b37c4b18d10ec5b34d9e00028a3aaf286
Nam-Yeonhwa <nyh2ya@gmail.com>
2025-08-04 15:39:33 +0900

    feat(Panel):tag url 한글 깨지지 않도록 수정

--- src/app/_component/common/Panel.tsx
+++ src/app/_component/common/Panel.tsx
@@ -5,45 +5,47 @@ import { sortedTotalPostlist, totalPostlist } from 'data/post_db';
 /**
  * totalPostlist의 tag[] 문자열을 중복하지 않고 결과값을 리턴
  */
-const uniqueTags = Array.from(
-  new Set(totalPostlist.flatMap((post) => post.tag)),
-);
+const uniqueTags = Array.from(new Set(totalPostlist.flatMap(post => post.tag)));
 
 export default function Panel() {
-  return (
-    <aside className={style.panel_wrapper}>
-      <div className={style.access}>
-        {/* 최신게시물 : start */}
-        <section className={style.access_lastmod}>
-          <h2 className={style.panel_heading}>Recently Updated</h2>
-          <ul className={style.panel_recently}>
-            {sortedTotalPostlist.slice(0, 5).map((value) => {
-              return (
-                <li key={value.id} className={style.text_truncate}>
-                  <Link href={`/posts/${value.id}`}>{value.title}</Link>
-                </li>
-              );
-            })}
-          </ul>
-        </section>
-        {/* 최신게시물 : end */}
-        {/* 트렌딩 태그 : start */}
-        <section className={style.panel_tag_contain}>
-          <h2 className={style.panel_heading}>Trending Tags</h2>
-          <div className={style.panel_tag}>
-            {uniqueTags.map((value, id) => {
-              return (
-                value && (
-                  <Link key={id} className={style.tag} href={`/tags/${value}`}>
-                    {value}
-                  </Link>
-                )
-              );
-            })}
-          </div>
-        </section>
-        {/* 트렌딩 태그 : end */}
-      </div>
-    </aside>
-  );
+	return (
+		<aside className={style.panel_wrapper}>
+			<div className={style.access}>
+				{/* 최신게시물 : start */}
+				<section className={style.access_lastmod}>
+					<h2 className={style.panel_heading}>Recently Updated</h2>
+					<ul className={style.panel_recently}>
+						{sortedTotalPostlist.slice(0, 5).map(value => {
+							return (
+								<li key={value.id} className={style.text_truncate}>
+									<Link href={`/posts/${value.id}`}>{value.title}</Link>
+								</li>
+							);
+						})}
+					</ul>
+				</section>
+				{/* 최신게시물 : end */}
+				{/* 트렌딩 태그 : start */}
+				<section className={style.panel_tag_contain}>
+					<h2 className={style.panel_heading}>Trending Tags</h2>
+					<div className={style.panel_tag}>
+						{uniqueTags.map((value, id) => {
+							return (
+								value && (
+									<Link
+										key={id}
+										className={style.tag}
+										href={`/tags/${encodeURIComponent(value)}`}
+									>
+										{value}
+									</Link>
+								)
+							);
+						})}
+					</div>
+				</section>
+				{/* 트렌딩 태그 : end */}
+			</div>
+		</aside>
+	);
 }
